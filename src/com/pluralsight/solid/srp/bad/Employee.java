27d6f40c41cd6b05f03e737eb63867a87d62d7da
msanchez <msanchez@localhost>
2023-03-21 13:42:25 +0800

    SOLID: SRP.

---
+++ src/com/pluralsight/solid/srp/bad/Employee.java
@@ -0,0 +1,11 @@
+package com.pluralsight.solid.srp.bad;
+
+public class Employee {
+
+	public static void save(Employee employee) {
+		// Builds Employee record summary using StringBuilder
+		// Creates a File
+		// Writes Employee record summary to the File
+		// Logs results
+	}
+}
